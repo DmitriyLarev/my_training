33e510eaa1bb67bf42566f2dbe1f20f847455681
Dmitriy Larev <81909838+DLarev06@users.noreply.github.com>
2024-09-03 11:56:31 +0300

    Create module_1_2.py

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = "Python"
+time_per_task = hours_spent / completed_tasks
+
+print("Курс:", course_name + ",", "всего задач:", str(completed_tasks) + ",", "затрачено часов:", str(hours_spent) + ",", "среднее время выполнения", time_per_task, "часа")
