bc285a54a047ea21b14be6af840f02fd703d8413
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-09-04 12:12:23 +0300

    task done

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {"Dima": 1997, "Sasha": 2000, "Yura": 1980}
+print(my_dict)
+print(my_dict["Dima"])
+print(my_dict.get("Lala"))
+my_dict.update({"Sam": 1999, "Ivan": 1988})
+print(my_dict.pop("Sasha"))
+print(my_dict)
+
+my_set = {1, 2, 3, "String", False, 12, 1, 2, 3, "String"}
+print(my_set)
+my_set.update({5, 7})
+my_set.pop()
+print(my_set)
+
