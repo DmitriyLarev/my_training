30139ecfdebf9a067143c11e2113a60a47e9014e
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-09-14 10:16:06 +0300

    module_3_1

---
+++ module_3_1.py
@@ -0,0 +1,23 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    string_tuple = (len(string), string.upper(), string.lower())
+    return string_tuple
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    for i in list_to_search:
+        if string == i.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
