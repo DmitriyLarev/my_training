2d468d3fb52826dcdfc93c1518fa6bd3e8d8c981
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-10-04 14:31:26 +0300

    add module_5_4.py

---
+++ module_5_4.py
@@ -0,0 +1,74 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self. number_of_floors}"
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+            return self
+
+    def __iadd__(self, other):
+        if isinstance(other, int):
+            return self.__add__(other)
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            return self.__add__(other)
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+
+print(House.houses_history)
