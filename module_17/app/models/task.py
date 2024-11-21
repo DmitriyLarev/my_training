dc1a30bfe9bb755599dd9b632e091d896bdf40be
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-11-21 19:52:54 +0300

    module_17: Модели SQLAlchemy

---
+++ module_17/app/models/task.py
@@ -0,0 +1,19 @@
+from module_17.app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from module_17.app.models import *
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer,  ForeignKey('users.id'), index=True, nullable=False)
+    slug = Column(String, unique=True, index=True)
+
+    user = relationship('User', back_populates='tasks')
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
