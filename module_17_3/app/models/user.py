4cecd9b728c5bb83fe28b1018c3d1e5c22c2f36d
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-11-22 00:17:31 +0300

    add module_17_3

---
+++ module_17_3/app/models/user.py
@@ -0,0 +1,18 @@
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
+from sqlalchemy.orm import relationship
+from app.models import *
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+
+    tasks = relationship('Task', back_populates='user')
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
